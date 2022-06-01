2e00041bcf2023e0dfa868d4aa10db507aa2ffef
Armen Deroian <cosmic5173@gmail.com>
2022-05-31 22:52:19 -0400

    Create AdvertiseSystem.cs

---
+++ protocol/AdvertiseSystem.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace RakLib.protocol; 
+
+public class AdvertiseSystem : Packet {
+	
+	public string ServerName { get; set; }
+
+	public override int GetPid() {
+		return MessageIdentifiers.ID_ADVERTISE_SYSTEM;
+	}
+
+	protected override void EncodePayload(PacketSerializer buffer) {
+		buffer.WriteString(ServerName);
+	}
+
+	protected override void DecodePayload(PacketSerializer buffer) {
+		ServerName = buffer.ReadString();
+	}
+}
